4217dfbf6ed63b4a7b6e412ef614c131117d911f
Matheesha-Nayanajith <nayanajithmatheesha@gmail.com>
2021-06-25 03:19:19 +0530

    first update

---
+++ pro01.R
@@ -0,0 +1,17 @@
+myString <- "Hello , World"
+print(myString)
+x=read.csv("rlec 03.csv", header = F)
+x
+str(x)
+x=read.csv("rlec 03.csv", header = T)
+x
+str(x)
+
+
+abc = "https://support.staffbase.com/hc/en-us/article_attachments/360009197031/username.csv"
+radio = read.table(abc, header = TRUE, sep = ",", stringsAsFactors = FALSE)
+str(radio)
+View(radio)
+get_yesterday <- function(){
+  return(Sys.Date() - 1)
+}
