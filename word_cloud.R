24feb5019287be92e685cb75f3c109a06bf8d76f
Matheesha-Nayanajith <nayanajithmatheesha@gmail.com>
2021-08-27 22:29:34 +0530

    update

--- word_cloud.R
+++ word_cloud.R
@@ -5,3 +5,20 @@ arrange(df, cyl, desc(disp))#arrange the rows in the descending order of disp an
 df %>% arrange(desc(wt))
 arrange(airquality, desc(Month), Day)#arrange the rows in the descending order of momth, and then in the ascending order of Day
 
+
+#summarise function ----
+?summarise 
+df %>% summarise(Mmpg = mean(mpg))
+df %>% summarise(disp = mean(disp),hp = mean(hp))
+df %>% group_by(am) %>% summarise(meands = mean(disp), n = n())#n() is the count - here no of cars
+
+df %>% group_by(am) %>% summarise(meanmpg = mean(mpg), n = n())
+
+#filter function ----
+?filter
+filter(df, cyl == 6)
+df %>% filter(carb>4)
+df %>% filter(mpg > mean(mpg))
+df %>% filter(cyl == 4,disp>90)
+head(airquality)
+range(df$disp)
