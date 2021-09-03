a00bf11c02d739337d124fcdeba9581e4e01c1d4
Matheesha-Nayanajith <nayanajithmatheesha@gmail.com>
2021-09-03 13:45:37 +0530

    Update.02.01

--- word_cloud.R
+++ word_cloud.R
@@ -49,3 +49,9 @@ df %>% group_by(cyl) %>% arrange((desc(mpg)))
 arrange(df, cyl, desc(disp))#arrange the rows in the descending order of disp and then in the ascending order of cyl
 df %>% arrange(desc(wt))
 arrange(airquality, desc(Month), Day)#arrange the rows in the descending order of momth, and then in the ascending order of Day
+
+
+
+#Testing 
+#Testing 
+#Testing
